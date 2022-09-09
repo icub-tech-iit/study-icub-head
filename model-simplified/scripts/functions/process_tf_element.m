ee9d00ec561635924aa2dad3b3b537f6edda8caa
Mattia Fussi <mattia.fussi@iit.it>
2022-09-09 17:21:54 +0200

    lil fix

--- model-simplified/scripts/functions/process_tf_element.m
+++ model-simplified/scripts/functions/process_tf_element.m
@@ -1,16 +1,17 @@
-function den = process_tf_element(p, threshold, par_prefix, uncertain_range)
-% checks if a list of linear system parameter is the same in the array 
-% according to threshold; if true add it as a polynomial root, otherwise
-% create an uncertain parameter and add it as a polynomial root
+function tf_poly = process_tf_element(p, threshold, par_prefix, uncertain_range)
+% checks if linear system parameter is the same in the array created by the
+% models; evaluation occurs according to threshold: 
+% if true add it as a polynomial root, otherwise
+% create an uncertain parameter and add it as a polynomial root:
 % If the parameter is a static gain add it as a multiplicative factor
     j = 1;
     s = tf('s'); 
-    den = tf(1);
+    tf_poly = tf(1);
     for i=1:size(p, 1)
         if any((abs(p(i, :) - p(1)) > threshold))
-                    nominal_root_index = intersect(find(p(i, :) ~=(min(p(i, :)))), ...
-                                       find(p(i, :) ~=(max(p(i, :)))) ...
-                                       );
+            nominal_root_index = intersect(find(p(i, :) ~=(min(p(i, :)))), ...
+                               find(p(i, :) ~=(max(p(i, :)))) ...
+                               );
             r = ureal(sprintf('%s_%d',par_prefix, j), mean(p(i, nominal_root_index)), 'percentage', uncertain_range);
             j = j + 1;
         else
@@ -18,9 +19,9 @@ function den = process_tf_element(p, threshold, par_prefix, uncertain_range)
         end
 
         if(strcmp(par_prefix,'gain') || strcmp(par_prefix, 'k'))
-             den = r;
+             tf_poly = r;
         else
-             den = den *  (s - r);
+             tf_poly = tf_poly *  (s - r);
         end
     end
 end
